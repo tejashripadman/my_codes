31f837c13fa9f7077c71e801d2f1a507664e95fc
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-07-18 20:31:55 +0530

    Create methods_to_reverse_list.py

---
+++ methods_to_reverse_list.py
@@ -0,0 +1,11 @@
+def reversed_list_1(l):
+  return l[::-1]
+
+def reversed_list_2(l):
+  return l.sort(reverse=True)
+
+def reversed_list_3(l):
+  return l.reverse()
+
+def reversed_list_4(l):
+  return(list(reversed(l))
