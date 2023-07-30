e17b30d9a82fb456c5c034644d884da1b80e637d
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-07-30 18:50:12 +0530

    Create multiplier.py

---
+++ multiplier.py
@@ -0,0 +1,6 @@
+def multipler(n):
+  """It accepts the input in format of string and returns integer value of n+nn+nnn"""
+    n = str(n)
+    sum_1 = int(n*2)
+    sum_2 = int(n*3)
+    return(int(n)+sum_1+sum_2)
