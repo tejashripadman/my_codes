4dbfb92c20d44227102513d9e00116646e17471a
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-08-14 20:01:29 +0530

    Update uncommon_words_finder_2.py

--- uncommon_words_finder_2.py
+++ uncommon_words_finder_2.py
@@ -1,6 +1,4 @@
 def uncommon_word_set(A,B):
-    a = A.split()
-    b = B.split()
-    l = set([i for i in a if i not in b])
-    l1 = set([i for i in b if i not in a])
-    return(list(l.symmetric_difference(l1)))
+    a = set(A.split())
+    b = set(B.split())
+    return(list(a.symmetric_difference(b)))
