a9e49f9e1409af4aa44dc5ff9551e6a1f40aa981
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-08-16 20:16:39 +0530

    Create character_repitition.py

---
+++ character_repitition.py
@@ -0,0 +1,8 @@
+def character_repition(list_inp,element,count):
+    '''This function takes list of list or list of tuples and returns a list if any of the elements in the input list containts 
+    specified element with given number of occurance'''
+    l=[]
+    for i in list_inp:
+        if element in i and i.count(element) == count:
+            l.append(i)
+    return l
