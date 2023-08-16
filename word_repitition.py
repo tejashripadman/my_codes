685df3d4a659ae0a9c003f8dc42c4cee02b91eaa
tejashripadman <110733090+tejashripadman@users.noreply.github.com>
2023-08-16 20:07:54 +0530

    Create word_repitition.py

---
+++ word_repitition.py
@@ -0,0 +1,3 @@
+def word_repitition(l1,l2):
+  '''This function takes l1 as list of any type of data and l2 with list of position numbers to be repeated and returns a list with repeated elements'''
+    return [l1[i] for i in l2]
